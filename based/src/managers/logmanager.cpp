433e7931bd7c83ddcaeae34cd093ddd710ac8f97
Jake Morgenstern <jmorgenstern15@gmail.com>
2025-04-10 19:06:48 -0400

    Added file logging

--- based/src/managers/logmanager.cpp
+++ based/src/managers/logmanager.cpp
@@ -2,6 +2,7 @@
 #include "managers/logmanager.h"
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
 
 namespace based::managers
 {
@@ -12,6 +13,9 @@ namespace based::managers
 		consoleSink->set_pattern("%^[%Y-%m-%d %H:%M:%S.%e] %v%$");
 
 		std::vector<spdlog::sink_ptr> sinks{ consoleSink };
+#ifdef BASED_CONFIG_DEBUG
+		sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Logs/recent_log.txt"));
+#endif
 		auto logger = std::make_shared<spdlog::logger>(BASED_DEFAULT_LOGGER_NAME, sinks.begin(), sinks.end());
 		logger->set_level(spdlog::level::trace);
 		logger->flush_on(spdlog::level::trace);
